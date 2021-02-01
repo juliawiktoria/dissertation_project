ef7a641fdc07c0a348f6a6887d7fb9fea09324c3
juliawiktoria <maybe.nott123@gmail.com>
2021-02-01 20:04:27 +0000

    simple discriminator

--- discriminator.py
+++ discriminator.py
@@ -9,4 +9,17 @@ from torchvision import transforms
 
 # class for the discriminator NN
 class Discriminator:
-    pass
+    def __init__(self, inp, out):
+        super(Discriminator, self).__init__()
+
+        self.net = nn.Sequential(nn.Linear(inp, 300), 
+								 nn.ReLU(inplace=True),
+								 nn.Linear(300, 300),
+								 nn.ReLU(inplace=True),
+								 nn.Linear(300, 200),
+								 nn.ReLU(inplace=True),
+								 nn.Linear(200, out),
+                                 nn.Sigmoid())
+    def forward_pass(self, x):
+        x = self.net(x)
+        return x

2acc5b4eb29e4029528d0225d84d88f87aa26fa3
juliawiktoria <maybe.nott123@gmail.com>
2021-02-01 20:01:42 +0000

    simple generator

--- generator.py
+++ generator.py
@@ -9,4 +9,15 @@ from torchvision import transforms
 
 # class for the generator NN
 class Generator(nn.Module):
-    pass
+	def __init__(self, inp, out):
+		super().__init__()
+		self.net = nn.Sequential(nn.Linear(inp, 300), 
+								 nn.ReLU(inplace=True),
+								 nn.Linear(300, 1000),
+								 nn.ReLU(inplace=True),
+								 nn.Linear(1000, 800),
+								 nn.ReLU(inplace=True),
+								 nn.Linear(800, out))
+	def forward_pass(self, x):
+		x = self.net(x)
+		return x

b02a629aa978f7476c195cecbf8bcf58c4cc2043
juliawiktoria <maybe.nott123@gmail.com>
2021-02-01 17:31:34 +0000

    added imports

--- discriminator.py
+++ discriminator.py
@@ -1,3 +1,12 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils import data as t_data
+import torchvision.datasets as datasets
+from torchvision import transforms
+
 # class for the discriminator NN
 class Discriminator:
     pass

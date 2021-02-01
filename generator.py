c406a0b8f082451162ad3da29cbcd3a5587bc408
juliawiktoria <maybe.nott123@gmail.com>
2021-02-01 21:07:47 +0000

    minor fix i forgot parameters

--- generator.py
+++ generator.py
@@ -1,16 +1,12 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.autograd import Variable
-from torch.utils import data as t_data
-import torchvision.datasets as datasets
-from torchvision import transforms
 
 # class for the generator NN
 class Generator(nn.Module):
 	def __init__(self, inp, out):
-		super().__init__()
+		super(Generator, self).__init__()
+		
 		self.net = nn.Sequential(nn.Linear(inp, 300), 
 								 nn.ReLU(inplace=True),
 								 nn.Linear(300, 1000),
